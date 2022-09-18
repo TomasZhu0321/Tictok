19002ded0f9fa1b4e58ded1f6d64c771646f71be
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-18 10:47:54 -0400

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,22 +3,22 @@ import Logo from "../utils/logo.png";
 import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { GoogleLogin,GoogleLogout } from "react-google-login";
-import {AiOutlineLogout} from 'react-icons/ai';
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
-import {IoMdAdd} from 'react-icons/io';
+import { IoMdAdd } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between items-center border-b-2 border-black py-2 px-4">
+    <div className="w-full flex justify-between items-center border-black border-b-2 py-2 px-4">
       <Link href="/">
-        <div className="w-[70px] h-[80px] md:w-[90px] md:h-[100px]">
+        <div className="w-[70px] h-[80px] md:w-[90px] md:h-[110px]">
           <Image
-            className="cursor-pointer"
             src={Logo}
+            className="pointer-cursor"
             alt="logo"
             layout="responsive"
-          />
+          ></Image>
         </div>
       </Link>
     </div>
