909c2244b1203c6c33a0a0366e6e1cd79594d22f
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-18 02:09:31 -0400

    css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app' 
-import {useState, useEffect} from 'react'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
-const MyApp = ({ Component, pageProps }: AppProps)=> {
-  const [isSSR,setIsSSR] = useState(true);
-  useEffect(()=>{
-    setIsSSR(false)
-  },[])
-  if(isSSR) return null;
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // server-side rendering check
+  const [isSSR, setIsSSR] = useState(true);
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+  if (isSSR) return null;
 
   return (
     <div>
       <Navbar />
-      <div className='flex gap-6 md:gap-20'>
-        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-          <Sidebar />
-        </div>
-        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
-          <Component {...pageProps} />
-        </div>
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[90vh] overflow-hidden xl:hover:overflow-auto"><Sidebar /></div>
+        <div className="h-[88vh] mt-4 flex flex-col videos overflow-auto gap-10 flex-1"><Component {...pageProps} /></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyApp
+export default MyApp;
+
+
+// <div>
+    //   <Navbar />
+    //   <div className='flex gap-6 md:gap-20'>
+    //     <div className='h-[90vh] overflow-hidden xl:hover:overflow-auto'>
+    //       <Sidebar />
+    //     </div>
+       
+    //     <div className='mt-4  flex flex-col gap-10  overflow-auto h-[88vh] videos flex-1'>
+    //       <Component {...pageProps} />
+    //     </div>
+       
+       
+    //   </div>
+    // </div>
+    
