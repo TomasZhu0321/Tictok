007a295d277bcb102244d79533c2a2fd65999f8b
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-20 00:59:40 -0400

    add video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -14,11 +14,20 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
   const [isHover, setHover] = useState(false);
   const [playing, setPlaying] = useState(false);
   const [muted, setVideMute] = useState(false);
-  const videoRef = useRef();
+  const videoRef = useRef<HTMLVideoElement>(null);  //similar to document.getElementByid;
+  const onVideoPres = ()=>{
+    if(playing){
+      videoRef?.current?.pause();
+      setPlaying(false);
+    }else{
+      videoRef?.current?.play(); 
+      setPlaying(true);
+    }
+  }
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
-        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded ">
           <div className="md:w-16 md:h-16 w-10 h-10">
             <Link href="/">
               <>
@@ -57,28 +66,30 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
             <Link href="/">
               <video
                 loop
+                ref={videoRef}
                 className="lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
-                src={post.video.asset.url}
-              ></video>
+               >
+                <source src={post.video.asset.url}></source>
+               </video>
             </Link>
             {isHover && (
-              <div>
+              <div className='absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] lg:w-[600px] p-3'>
                 {playing ? (
-                  <button>
-                    <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
+                  <button onClick={onVideoPres} className="rounded-full bg-slate-100 p-1">
+                    <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
                   </button>
                 ) : (
-                  <button>
-                    <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
+                  <button  onClick={onVideoPres} className="rounded-full bg-slate-100 p-1">
+                    <BsFillPlayFill className="text-black text-2xl lg:text-4xl" />
                   </button>
                 )}
 
                 {muted ? (
-                  <button>
+                  <button onClick={() => setVideMute(false)} className="rounded-full bg-slate-100 p-1">
                     <HiVolumeUp className="text-black text-2xl lg:text-4xl" />
                   </button>
                 ) : (
-                  <button>
+                  <button onClick={() => setVideMute(true)} className="rounded-full bg-slate-100 p-1">
                     <HiVolumeOff className="text-black text-2xl lg:text-4xl" />
                   </button>
                 )}
