58739eab413f08227436a5fb6014a39900039756
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-19 20:29:20 -0400

    video card

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const List = ({ list,mt }: { list: string[],mt:boolean}) => (
+    <div className={`flex flex-wrap gap-2 ${mt && 'mt-5'}`}>
+      {list.map((item) => (
+        <p
+          key={item}
+          className="text-gray-300 text-sm hover:underline cursor-pointer"
+        >
+          {item}
+        </p>
+      ))}
+    </div>
+  );
 
-export default Footer
+  return (
+    <div className="mt-6 hidden xl:block">
+      <List list={footerList1} mt={false}></List>
+      <List list={footerList2} mt></List>
+      <List list={footerList3} mt></List>
+    </div>
+  );
+};
+
+export default Footer;
