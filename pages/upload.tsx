8d7177e4b52d61a0ab4191acc530b89ba608b76e
ThomasZhu <tomaszhu0321@gmail.com>
2022-09-24 19:40:34 -0400

    detail setting

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -9,6 +9,7 @@ import useAuthStore from '../store/authStore';
 // import { BASE_URL } from '../utils';
 import { client } from '../utils/client';
 import { topics } from '../utils/constants';
+import { BASE_URL } from '../utils';
 
 const Upload = () => {
   const [caption, setCaption] = useState('');
@@ -71,7 +72,7 @@ const Upload = () => {
         topic,
       };
 
-      await axios.post('http://localhost:3000/api/post', doc);
+      await axios.post(`${BASE_URL}/api/post`, doc);
         
       router.push('/');
     }
